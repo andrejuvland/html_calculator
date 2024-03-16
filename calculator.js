5a7107d0b4e678f1fd12fe1dcfcbe3162b1106b1
andrejuvland <andre_juvland@hotmail.com>
2024-03-16 21:07:38 +0100

    display number and decimal

--- calculator.js
+++ calculator.js
@@ -1,1 +1,32 @@
+const calculator = document.querySelector('.calculator');
+const keys = calculator.querySelector('.calculator_keys');
+const display = document.querySelector('.calculator_display');
 
+
+keys.addEventListener('click', e => {
+    if(e.target.matches('button')) {
+        const key = e.target;
+        const action = key.dataset.action;
+        const keyContent = key.textContent;
+        const displayNumber = display.textContent;
+        if(!action) {
+            if(displayNumber === '0') {
+                display.textContent = keyContent;
+            } else {
+                display.textContent = displayNumber + keyContent;
+            }
+        }
+        if(action === 'add' || action === 'subtract' || action === 'divide' || action === 'multiply') {
+            console.log('operator key!')
+        }
+        if(action === 'decimal') {
+            display.textContent = displayNumber + '.';
+        }
+        if(action === 'clear') {
+            display.textContent = '0';
+        }
+        if(action === 'calculate') {
+            console.log('equal key')
+        }
+    }
+})
